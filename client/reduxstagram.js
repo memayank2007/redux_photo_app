667b15985cd1537a604386ac41cc74c508d150c1
memayank2007 <memayank2007@gmail.com>
2016-10-22 13:05:46 +0530

    connect store and actions

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -3,23 +3,27 @@ import { render } from 'react-dom';
 
 import css from './styles/style.styl';
 
-import Main from './components/Main';
+import App from './components/App';
 
 import Single from './components/Single';
 
 import PhotoGrid from './components/PhotoGrid';
 
+
 import { Router , Route , IndexRoute , browserHistory } from 'react-router';
 
+import {Provider} from 'react-redux';
+import store , {history} from './store.js';
 
 const router =(
-	<Router history={browserHistory}>
-		<Route path='/' component={Main}>
-			<IndexRoute component={PhotoGrid}></IndexRoute>
-			<Route path="/views/:photId" component={Single}></Route>
-		</Route>
-	</Router>
-
+	<Provider store={store}>
+		<Router history={history}>
+			<Route path='/' component={App}>
+				<IndexRoute component={PhotoGrid}></IndexRoute>
+				<Route path="/views/:photId" component={Single}></Route>
+			</Route>
+		</Router>
+	</Provider>
 	)
 
 
